384731493f9caf4cf64264e943238172af2cb858
theblvckdev <nathancodes05@gmail.com>
2024-06-24 22:14:39 +0100

    achivement cards component

---
+++ src/components/ui/achivement_cards.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from "react";
+
+interface AchivementCardPropsType {
+  icon: ReactElement;
+  title: string;
+  amount: number;
+}
+
+const AchivementCard = ({ amount, icon, title }: AchivementCardPropsType) => {
+  return (
+    <>
+      <div className="bg-white p-5 shadow-spread rounded flex flex-col space-y-2">
+        <div>{icon}</div>
+        <div className="text-sm uppercase text-secondary font-primary">
+          {title}
+        </div>
+        <h3 className="text-3xl text-secondary font-semibold font-primary">
+          {amount.toLocaleString()}
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default AchivementCard;
