493f8af310aeb568e5237df99fce50c95b3fcfb3
theblvckdev <nathancodes05@gmail.com>
2024-06-25 10:48:22 +0100

    code update for responsive design

--- src/components/ui/achivement_cards.tsx
+++ src/components/ui/achivement_cards.tsx
@@ -11,10 +11,10 @@ const AchivementCard = ({ amount, icon, title }: AchivementCardPropsType) => {
     <>
       <div className="bg-white p-5 shadow-spread rounded flex flex-col space-y-2">
         <div>{icon}</div>
-        <div className="text-sm uppercase text-secondary font-primary">
+        <div className="xl:text-sm text-[11px] uppercase text-secondary font-secondary">
           {title}
         </div>
-        <h3 className="text-2xl text-secondary font-semibold font-primary">
+        <h3 className="text-2xl text-secondary font-semibold font-secondary">
           {amount.toLocaleString()}
         </h3>
       </div>
